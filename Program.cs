f8ad7e62deffcbc720a144e0a43775bac8348168
Vladimir Akopyan <vladimir@akopyan.me>
2016-11-29 11:33:19 +0000

    WebSocketServer

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using System.Security.Cryptography.X509Certificates;
+
+namespace EdisonBrick
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            X509Certificate2 cert = new X509Certificate2("YourFileName.pfx",
+    "YOURPWD");
+            var host = new WebHostBuilder()
+                .UseKestrel(cfg => cfg.UseHttps('www_quickbird_uk.crt',))
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
---
+++ SocketHandler.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.WebSockets;
+using System.Threading;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace EdisonBrick
+{
+
+
+    public class SocketHandler
+    {
+        public const int BufferSize = 4096;
+        System.IO.Ports.SerialPort 
+
+        WebSocket socket;
+
+        SocketHandler(WebSocket socket)
+        {
+            this.socket = socket;
+        }
+
+
+
+        async Task EchoLoop()
+        {
+            var buffer = new byte[BufferSize];
+            var seg = new ArraySegment<byte>(buffer);
+
+            while (this.socket.State == WebSocketState.Open)
+            {
+                var incoming = await this.socket.ReceiveAsync(seg, CancellationToken.None);
+                var outgoing = new ArraySegment<byte>(buffer, 0, incoming.Count);
+                await this.socket.SendAsync(outgoing, WebSocketMessageType.Text, true, CancellationToken.None);
+            }
+        }
+
+        static async Task Acceptor(HttpContext hc, Func<Task> n)
+        {
+            if (!hc.WebSockets.IsWebSocketRequest)
+                return;
+
+            var socket = await hc.WebSockets.AcceptWebSocketAsync();
+            var h = new SocketHandler(socket);
+            await h.EchoLoop();
+        }
+
+        public static void Map(IApplicationBuilder app)
+        {
+            app.UseWebSockets();
+            app.Use(SocketHandler.Acceptor);
+        }
+    }
+}
